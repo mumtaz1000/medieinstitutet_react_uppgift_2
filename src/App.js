c92601b926c7ddc3ee3812c8aa781b4b4f0a7bca
Mumtaz Fatima <shereenfatima1000@gmail.com>
2021-04-19 00:13:14 +0200

    Homepage link edit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import Contact from "./pages/Contact"
 import Recipe from "./pages/Recipedata"
-import Card from "./components/Card"
+
 
 class App extends React.Component {
   constructor(){
@@ -15,9 +15,8 @@ class App extends React.Component {
   return (
     <div class="bg-indigo-100"> 
     <Navbar />
-   
     <Switch>
-        <Route exact path="/" component={Home}/>
+        <Route exact path={process.env.PUBLIC_URL + '/'} component={Home}/>
 
         <Route path="/contact" component={Contact }/>
         <Route path="/recipe" component={Recipe} />
